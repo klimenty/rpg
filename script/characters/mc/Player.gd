8ad669a5c020040d9c0f7788e1ab1edaee75188e
klimenty <filklym@gmail.com>
2024-08-11 17:15:15 +0300

    Movement speed now sets based on MovementState. Deleted some comments

--- script/characters/mc/Player.gd
+++ script/characters/mc/Player.gd
@@ -1,53 +1,47 @@
 extends Node2D
 
-var key_buffer: Array[String] = [] #Store all movement Inputs. Last one is used to update direction
-var direction_vector: Vector2 = Vector2.ZERO #Store direction value for movement
-@export var tile_map: TileMap #Store tilemap. TileMap is needed for movement so game will crash if you don't link it in scene
-@export var sprite_2d: Sprite2D #Store character sprite. It moves to the Player position
-@export var ray_cast_2d: RayCast2D #Store RayCast2D. It's used to check if next tile is free before moving
-var is_moving: bool = false #If is True, lock player movement and actions. But you still can sprint or sneaking
-var speed: float = 3.0 #This variable used for movement. Value can be altered based on user input
-var cell_size: int = 64 #Store cell length. It used for RayCast2D
+#Store all movement Inputs. Last one is used to update direction
+var key_buffer: Array[String] = []
+#Store direction value for movement
+var direction_vector: Vector2 = Vector2.ZERO
+#Store tilemap. TileMap is needed for movement so game will crash if you don't link it in scene
+@export var tile_map: TileMap
+@export var sprite_2d: Sprite2D
+@export var ray_cast_2d: RayCast2D
+#If is True, lock player movement and actions. But you still can sprint or sneaking
+var is_moving: bool = false
+var speed: float = 3.0
+var cell_size: int = 64
 @export var walking_speed: float = 3.0
 @export var running_speed: float = 6.0
 @export var sneaking_speed: float = 1.5
-var is_running: bool = false
-var is_sneaking: bool = false
 var action_buffer: Array[String] = []
-enum MovementStaes {
-	IDLE = 0,
+enum MovementStates {
 	WALKING,
 	RUNNING,
 	SNEAKING
 }
-var MovementStae = MovementStaes.IDLE
+var MovementState = MovementStates.WALKING
 
 
 func _ready() -> void:
-	#Set default speed to walking speed at the start of scene
-	speed = walking_speed
+	pass
 
 
 #Handles all player inputs
 func _input(event: InputEvent) -> void:
-	#Check if Shift is pressed and modify movement speed
+	#Enable and disable sprint
 	if event.is_action_pressed("shift"):
-		speed = running_speed
-
-	#Check if Shift was released and modify movement speed
+		MovementState = MovementStates.RUNNING
 	if event.is_action_released("shift"):
-		speed = walking_speed
+		MovementState = MovementStates.WALKING
 
-	#Check if Ctrl is pressed and modify movement speed
+	#Enable and disable sneaking
 	if event.is_action_pressed("ctrl"):
-		if is_sneaking == true:
-			speed = walking_speed
-			is_sneaking = false
+		if MovementState == MovementStates.SNEAKING:
+			MovementState = MovementStates.WALKING
 		else:
-			speed = sneaking_speed
-			is_sneaking = true
-				
-		
+			MovementState = MovementStates.SNEAKING
 
 	#Store all pressed movement buttons to key_buffer variable
 	if event.is_action_pressed("up"):
@@ -72,27 +66,36 @@ func _input(event: InputEvent) -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	#Change speed based on MovementState
+	move_state_machine()
 
 	#Checks if player is currently in moving animation.	If True, play movement animation and stop it when sprite reaches player position.
 	if is_moving:
 		#Move sprite_2d to player position. Speed is determined by speed variable. 
-		#If should me above destination check to prevent movement stuttering
+		#If should be above destination check to prevent movement stuttering
 		sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, speed)
-		#Check if sprite2D reached player position. If True, movement cycle is over and player movement is unlocked
 		if global_position == sprite_2d.global_position:
 			is_moving = false
 			return
 
 	#Checks if player is _not_ in moving animation.	
 	if not is_moving:
-		#If there is inputs in key_buffer, call function to transform input to Vector2
 		if key_buffer.size() > 0:
 			update_direction()
-		#If there is no inputs, set direction to zero (player stays in it's place)
 		else:
 			direction_vector = Vector2.ZERO
-		#Move character based on direction. If direction is Zero, players stays on current tile
 		move(direction_vector)
+
+
+#Change speed based on MovementState
+func move_state_machine():
+	match MovementState:
+		MovementStates.WALKING:
+			speed = walking_speed
+		MovementStates.RUNNING:
+			speed = running_speed
+		MovementStates.SNEAKING:
+			speed = sneaking_speed
 
 
 #Transform last string valuse from key_buffer to Vector2 values and store it in direction_vector
@@ -126,11 +129,9 @@ func move(direction: Vector2) -> void:
 	#If target tile is not walkable, stop function
 	if tile_data == null or tile_data.get_custom_data("walkable") == false:
 		return
-	#Point RayCast2D to next tile based on direction and cell size
 	ray_cast_2d.target_position = direction_vector * cell_size
 	#Update RayCast2D direction. Without this direction will change in the next cycle
 	ray_cast_2d.force_raycast_update()
-	#Don't allow to move player if RayCast2D collides with enemy or object
 	if ray_cast_2d.is_colliding():
 		return
 	#lock player movement
